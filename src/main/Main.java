1163a0a6e1e8c0f8eeb256433cad35a80b5f3c2c
Grigory Kislin <gkislin@yandex.ru>
2014-12-12 23:02:46 +0300

    OOP sample

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,9 +7,15 @@ package main;
 public class Main {
     /**
      * First java program
+     *
      * @param args : program arguments
      */
     public static void main(String[] args) {
-        System.out.format("Hello %s!", args[0]);
+        System.out.format("Hello %s!\n", args[0]);
+        Car raceCar = new RaceCar();
+        Car simpleCar = new SimpleCar();
+        System.out.println(raceCar.getSpeed());
+        raceCar.getDescription();
+        simpleCar.getDescription();
     }
 }
---
+++ src/ru/javawebinar/webapp/model/Resume.java
@@ -0,0 +1,13 @@
+package ru.javawebinar.webapp.model;
+
+import java.util.Collection;
+
+/**
+ * gkislin
+ * 12.12.2014.
+ */
+public class Resume {
+    String fullName;
+    String[] some;
+    Collection<String> collection;
+}
