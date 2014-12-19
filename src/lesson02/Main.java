eae970b94958c266721b3069b4727a8071b048f8
gkislin@yandex.ru <github13>
2014-12-19 21:49:39 +0300

    Implement equals

--- src/lesson02/Main.java
+++ src/lesson02/Main.java
@@ -1,11 +1,19 @@
 package lesson02;
 
+import ru.javawebinar.webapp.model.Link;
+
 /**
  * GKislin
  * 19.12.2014.
  */
 public class Main {
     public static void main(String[] args) {
-
+        int i= 5;
+        Link l1 = new Link("JavaWebinar", "javawebinar.ru");
+        Link l2 = l1;
+        Link l3 = new Link(l1);
+        System.out.println(l1.getClass());
+        System.out.println(l1.equals(l2));
+        System.out.println(l1.equals(l3));
     }
 }
--- src/ru/javawebinar/webapp/model/Link.java
+++ src/ru/javawebinar/webapp/model/Link.java
@@ -12,4 +12,29 @@ public class Link {
         this.name = name;
         this.url = url;
     }
+
+    public Link(Link link) {
+        this.name = link.name;
+        this.url = link.url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Link link = (Link) o;
+
+        if (name != null ? !name.equals(link.name) : link.name != null) return false;
+        if (!url.equals(link.url)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + url.hashCode();
+        return result;
+    }
 }
