3f4ea25da69e161302a39b5fd132b3b274787259
gkislin@yandex.ru <github13>
2014-12-19 20:54:18 +0300

    Rename package Add Link

---
+++ src/lesson02/Main.java
@@ -0,0 +1,11 @@
+package lesson02;
+
+/**
+ * GKislin
+ * 19.12.2014.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- src/ru/javawebinar/webapp/model/Link.java
+++ src/ru/javawebinar/webapp/model/Link.java
@@ -5,7 +5,11 @@ package ru.javawebinar.webapp.model;
  * 19.12.2014.
  */
 public class Link {
-    private String name;
-    private String url;
+    private final String name;
+    private final String url;
 
+    public Link(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
 }
