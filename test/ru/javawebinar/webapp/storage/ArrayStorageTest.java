399c60a71534697f33ed36d1e09d4e88cddd957f
gkislin@yandex.ru <github13>
2014-12-26 22:30:41 +0300

    Add JUnit

---
+++ test/ru/javawebinar/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,41 @@
+package ru.javawebinar.webapp.storage;
+
+import static org.junit.Assert.*;
+
+public class ArrayStorageTest {
+
+    @org.junit.Test
+    public void testClear() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSave() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testLoad() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetAllSorted() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSize() throws Exception {
+
+    }
+}
