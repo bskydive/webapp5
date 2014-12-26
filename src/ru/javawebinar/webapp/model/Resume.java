7cef036388ba0043ee0f5d0178c91429688cb5ef
gkislin@yandex.ru <github13>
2014-12-26 22:23:03 +0300

    Add resume constructors

--- src/ru/javawebinar/webapp/model/Resume.java
+++ src/ru/javawebinar/webapp/model/Resume.java
@@ -1,6 +1,8 @@
 package ru.javawebinar.webapp.model;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * gkislin
@@ -11,10 +13,15 @@ public class Resume {
     private String fullName;
     private String location;
     private String homePage;
-    private List<Contact> contacts;
-    private List<Section> sections;
+    private List<Contact> contacts = new LinkedList<>();
+    private List<Section> sections = new LinkedList<>();
 
     public Resume(String fullName, String location) {
+        this(UUID.randomUUID().toString(), fullName, location);
+    }
+
+    public Resume(String uuid, String fullName, String location) {
+        this.uuid = uuid;
         this.fullName = fullName;
         this.location = location;
     }
@@ -29,6 +36,38 @@ public class Resume {
         if (!uuid.equals(resume.uuid)) return false;
 
         return true;
+    }
+
+    public void addSection(Section section) {
+        sections.add(section);
+    }
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getHomePage() {
+        return homePage;
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public List<Section> getSections() {
+        return sections;
     }
 
     @Override
