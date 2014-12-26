78c0961f5a5763c05303681fe23e2637516b4aa3
gkislin@yandex.ru <github13>
2014-12-26 22:17:29 +0300

    Mock array implementation

--- src/ru/javawebinar/webapp/model/Resume.java
+++ src/ru/javawebinar/webapp/model/Resume.java
@@ -7,10 +7,32 @@ import java.util.List;
  * 12.12.2014.
  */
 public class Resume {
+    private String uuid;
     private String fullName;
     private String location;
     private String homePage;
     private List<Contact> contacts;
     private List<Section> sections;
 
+    public Resume(String fullName, String location) {
+        this.fullName = fullName;
+        this.location = location;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Resume resume = (Resume) o;
+
+        if (!uuid.equals(resume.uuid)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return uuid.hashCode();
+    }
 }
