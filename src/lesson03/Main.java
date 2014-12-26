d2c1f2df69f1fa05f1e1b6866183bd87fe0f421d
gkislin@yandex.ru <github13>
2014-12-26 21:44:52 +0300

    Reflection sample

--- src/lesson03/Main.java
+++ src/lesson03/Main.java
@@ -19,7 +19,19 @@ public class Main {
         Field f = Link.class.getDeclaredField("url");
         f.setAccessible(true);
         Link l = new Link("fghfh", "URL");
+        System.out.println(l instanceof Link);
+        System.out.println(Link.class.isInstance(l));
         System.out.println(f.get(l));
         System.out.println(l.getUrl());
+
+        String a1 = "Ja" + "va";
+        String a2 = "Java";
+        System.out.println(a1 == a2);
+
+        StringBuilder fill = new StringBuilder();
+        for (int i = 0; i < 1000; i++) {
+            fill.append("a");
+        }
+        System.out.println(fill.toString());
     }
 }
