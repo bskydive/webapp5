23ed88a77a73751773c8c0016d8add621f26bdba
gkislin@yandex.ru <github13>
2014-12-26 20:27:00 +0300

    Add ContactType

--- src/ru/javawebinar/webapp/model/Contact.java
+++ src/ru/javawebinar/webapp/model/Contact.java
@@ -5,15 +5,15 @@ package ru.javawebinar.webapp.model;
  * 19.12.2014.
  */
 public class Contact {
-    private final String type;
+    private final ContactType type;
     private final String value;
 
-    public Contact(String type, String value) {
+    public Contact(ContactType type, String value) {
         this.type = type;
         this.value = value;
     }
 
-    public String getType() {
+    public ContactType getType() {
         return type;
     }
 
---
+++ src/ru/javawebinar/webapp/model/ContactType.java
@@ -0,0 +1,14 @@
+package ru.javawebinar.webapp.model;
+
+/**
+ * GKislin
+ * 26.12.2014.
+ */
+public enum ContactType {
+    PHONE,
+    MOBILE,
+    HOME_PHONE,
+    SKYPE,
+    MAIL,
+    ICQ
+}
