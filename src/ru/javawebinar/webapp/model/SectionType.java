6ff54a60cf6e28434953116f2832050847db760b
gkislin@yandex.ru <github13>
2014-12-26 21:07:53 +0300

    Enhance SectionType

--- src/ru/javawebinar/webapp/model/SectionType.java
+++ src/ru/javawebinar/webapp/model/SectionType.java
@@ -5,9 +5,19 @@ package ru.javawebinar.webapp.model;
  * 26.12.2014.
  */
 public enum SectionType {
-    OBJECTIVE,
-    ACHIEVEMENT,
-    QUALIFICATIONS,
-    EXPERIENCE,
-    EDUCATION
+    OBJECTIVE("Позиция"),
+    ACHIEVEMENT("Достижения"),
+    QUALIFICATIONS("Квалификация"),
+    EXPERIENCE("Опыт работы"),
+    EDUCATION("Образование");
+
+    private String title;
+
+    SectionType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
